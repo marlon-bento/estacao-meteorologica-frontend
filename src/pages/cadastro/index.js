1a49092ce604b3850ae8942dcd03e203c0b7cc44
marlon-bento <marlon_lion@hotmail.com>
2024-04-30 19:35:28 -0300

    concertando cadastro

--- src/pages/cadastro/index.js
+++ src/pages/cadastro/index.js
@@ -6,15 +6,18 @@ function Cadastro() {
     const [name, setName] = useState('');
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [isSubmitting, setIsSubmitting] = useState(false); // Estado para controlar o envio do formulário
     const navigate = useNavigate(); // Inicialize o useNavigate
 
     const handleCadastro = async (e) => {
         e.preventDefault();
 
-        if (!name.trim() || !username.trim() || !password.trim()) {
-            alert('Por favor, preencha todos os campos.');
+        if (!name.trim() || !username.trim() || !password.trim() || isSubmitting) {
+            // Verifique se já está enviando o formulário antes de permitir o envio novamente
             return;
         }
+
+        setIsSubmitting(true); // Marque como enviando
 
         const cadastroData = {
             name: name,
@@ -33,6 +36,7 @@ function Cadastro() {
             alert('Erro ao cadastrar. Por favor, tente novamente mais tarde.');
         }
 
+        setIsSubmitting(false); // Marque como não enviando
         setName('');
         setUsername('');
         setPassword('');
@@ -45,7 +49,8 @@ function Cadastro() {
                 <input className='formulario' type="text" placeholder='Nome' value={name} onChange={(e) => setName(e.target.value)} />
                 <input className='formulario' type="text" placeholder='Username' value={username} onChange={(e) => setUsername(e.target.value)} />
                 <input className='formulario' type="password" placeholder='Senha' value={password} onChange={(e) => setPassword(e.target.value)} />
-                <input className='botao-form' type="submit" value="Cadastrar" />
+                <input className='botao-form' type="submit" value={isSubmitting ? 'Enviando...' : 'Cadastrar'} disabled={isSubmitting} />
+                {/* Desabilite o botão enquanto estiver enviando */}
                 <h1 className='info-login'>Já possui conta? <Link className='link-login' to='/'>Logar</Link></h1>
             </form>
         </div>
